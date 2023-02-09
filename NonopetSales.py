04cc760ecb64891ece6cfc31f60b97bf5f5bdd6d
mishhuang <mhuang987@gmail.com>
2023-02-09 14:17:24 -0500

    Create a class that represents sale items

--- NonopetSales.py
+++ NonopetSales.py
@@ -2,3 +2,23 @@ class InvalidSalesItemError(Exception):
     """Represents an error when an input sales item is not in the system"""
     pass
 
+class SalesItem:
+    """Represents an item for sale on the Nonopet website with a name, wholesale cost, and selling price"""
+
+    def __init__(self, name, cost, price):
+        """Creates a new menu item with a specified name, wholesale cost, and selling price"""
+        self._name = name
+        self._cost = cost
+        self._price = price
+
+    def get_name(self):
+        """Returns item name"""
+        return self._name
+
+    def get_wholesale_cost(self):
+        """Returns item cost"""
+        return self._cost
+
+    def get_selling_price(self):
+        """Returns item price"""
+        return self._price
