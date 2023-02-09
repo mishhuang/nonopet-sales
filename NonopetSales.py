388243bbc65a79802dc18ce7d3996569fe90301a
mishhuang <mhuang987@gmail.com>
2023-02-09 14:29:27 -0500

    create a class to track daily sales

--- NonopetSales.py
+++ NonopetSales.py
@@ -3,7 +3,7 @@ class InvalidSalesItemError(Exception):
     pass
 
 class SalesItem:
-    """Represents an item for sale on the Nonopet website with a name, wholesale cost, and selling price"""
+    """Represents an item for sale with a name, wholesale cost, and selling price"""
 
     def __init__(self, name, cost, price):
         """Creates a new menu item with a specified name, wholesale cost, and selling price"""
@@ -22,3 +22,20 @@ class SalesItem:
     def get_selling_price(self):
         """Returns item price"""
         return self._price
+
+
+class SalesForDay:
+    """Represents the sales at a business for a particular day"""
+
+    def __init__(self, days, sales_dict):
+        """Creates a new sales for day object that specifies a particular day and a dictionary with the names and amounts of the items sold"""
+        self._days = days
+        self._sales_dict = sales_dict
+
+    def get_day(self):
+        """Returns the number of days the lemonade stand has been open so far"""
+        return self._days
+
+    def get_sales_dict(self):
+        """Returns a dictionary whose keys are the names of the items sold and whose values are the number of those items sold"""
+        return self._sales_dict
